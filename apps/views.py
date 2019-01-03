ee2fc1fdb6e635e060c9c69ade0ec9ed4a547882
zhiyugege <jhyxlzh@126.com>
2019-01-03 15:37:52 +0800

    2019-1-3-16-37-lzy

--- apps/views.py
+++ apps/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
+from django.http import JsonResponse
+from django.template import loader
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+	
+	return render(request, 'index.html')
