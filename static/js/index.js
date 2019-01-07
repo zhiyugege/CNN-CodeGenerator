4cf5574b46be93b2053d81309923cd711e688341
zhiyugege <jhyxlzh@126.com>
2019-01-08 07:52:58 +0800

    2019-1-8-8-25-lzy

--- static/js/index.js
+++ static/js/index.js
@@ -1,32 +1,77 @@
-// var states = new Bloodhound({
-//   datumTokenizer: function(d) { return Bloodhound.tokenizers.whitespace(d.word); },
-//   queryTokenizer: Bloodhound.tokenizers.whitespace,
-//   limit: 4,
-//   local: [
-//     { word: "Alabama" },
-//     { word: "Alaska" },
-//     { word: "Arizona" },
-//     { word: "Arkansas" },
-//     { word: "California" },
-//     { word: "Colorado" }
-//   ]
-// });
+$(document).ready(function(){
 
-// states.initialize();
+	$(".bg-gray").css({'border':'2px solid white','pointer-events':'none'});
 
-// $('input.tagsinput').tagsinput();
+	//获取卷积核信息
+	$(".convInfo").change(function(){
+		
+		var L = $("#convL").val();
+		if(L=="") L = "1";
+		var W = $("#convW").val();
+		if(W=="") W = "3";
+		var H = $("#convH").val();
+		if(H=="") H = "3";
+		var S = $("#convS").val();
+		if(S=="") S = "1";
+		var P = $("#convP").val();
+		if(P=="") P = "1";
+		var result = L+'.'+W+'.'+H+'.'+S+'.'+P
+		$("#"+GlobalId).find(".info").html(result);
+		console.log(GlobalId);
+	})
 
-// $('input.tagsinput-typeahead').tagsinput('input').typeahead(null, {
-//   name: 'states',
-//   displayKey: 'word',
-//   source: states.ttAdapter()
-// });
+	//获取池化层信息
+	$(".pool-input").change(function(){
 
-// $('input.typeahead-only').typeahead(null, {
-//   name: 'states',
-//   displayKey: 'word',
-//   source: states.ttAdapter()
-// });
+		var sign = "";
+		if($("#maxpool").is(":checked")) sign = "maxpool";
+		else sign = "avepool";
+		$("#"+GlobalId).find(".info").html(sign);
+
+	})
+
+	//获取标准化信息
+	$(".norm-input").change(function(){
+
+		var lag = ""
+		if($("#BN").is(":checked")) lag = "BN";
+		else lag = "GN";
+		$("#"+GlobalId).find(".info").html(lag);
+
+	})
+
+	//获取激活函数
+
+	$("#active-select").change(function(){
+
+		$("#"+GlobalId).find(".info").html($("#active-select").val());
+
+	})
+
+	//获取
+	$("#fully-input").change(function(){
+
+		$("#"+GlobalId).find(".info").html($("#fully-input").val());
+	})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
+
 
 
 
